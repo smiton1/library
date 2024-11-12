f71d5c0052ab9294205aac2782219bd8349209ec
smiton1 <182565156+smiton1@users.noreply.github.com>
2024-11-12 10:10:58 -0600

    added basic functionality

---
+++ app.js
@@ -0,0 +1,35 @@
+const myLibrary = [];
+const form= document.querySelector("form")
+const addBtn= document.querySelector(".add-btn").addEventListener("click",function(e){
+    e.preventDefault()
+    const bookTitle = document.getElementById("title").value
+    const bookAuthor = document.getElementById("author").value
+    const bookPages= document.getElementById("pages").value
+    let newBook = new Book(bookTitle, bookAuthor, bookPages)
+    myLibrary.push(newBook)
+    console.log(myLibrary)
+})
+ 
+function Book(title, author, pages) {
+    this.title=title,
+    this.author=author,
+    this.pages= pages
+}
+
+function addBookToLibrary() {
+
+}
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
