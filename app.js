3c700284b12179b6889590e74bf1a82ab8ea8da7
smiton1 <182565156+smiton1@users.noreply.github.com>
2024-11-14 09:23:08 -0600

    basics are done, could refactor and fix the checkbox toggle

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const myLibrary = [];
 const cardList = document.querySelector(".card-list")
 const form = document.querySelector("form")
 const dialog = document.querySelector("#dialogBox");
@@ -7,35 +6,45 @@ const closeButton = document.querySelector(".close");
 const removeBtn = document.querySelector(".closeBtn");
 const card = document.querySelector(".card");
 const addBtn= document.querySelector(".add-btn")
- 
-function Book(title, author, pages) {
+let counter = 0
+const myLibrary = []
+
+function Book(id, title, author, pages, read) {
+    this.id = id,
     this.title=title,
     this.author=author,
-    this.pages= pages
+    this.pages= pages,
+    this.read=read
 }
 
 function addBookToLibrary() {
     const bookTitle = document.getElementById("title").value
     const bookAuthor = document.getElementById("author").value
     const bookPages= document.getElementById("pages").value
+    const readBook = document.getElementById("read").value
     const div= document.createElement("div")
     const title = document.createElement("p")
     const author= document.createElement('p')
     const pages = document.createElement('p')
     const button = document.createElement('button')
+    const read = document.createElement("input")
+    read.setAttribute("type","checkbox" )
     button.textContent= "X"
     title.textContent =`Title: ${bookTitle} `
     author.textContent= `Author: ${bookAuthor}`
     pages.textContent= `Pages: ${bookPages}`
+    read.classList.add("toggle")
     button.classList.add("deleteBtn")
     button.addEventListener("click",()=>{
         deleteChild()
     })
     div.classList.add('card')
+    div.setAttribute("id", counter)
     div.append(button)
     div.append(title)
     div.append(author)
     div.append(pages)
+    div.append(read)
     cardList.appendChild(div)
 }
 
@@ -43,6 +52,8 @@ function deleteChild(){
     const cardList = document.querySelector(".card-list")
     const card = document.querySelector(".card");
     cardList.removeChild(card)
+    myLibrary.splice(card.id,1)
+
 }
 
 addBtn.addEventListener("click",function(e){
@@ -50,9 +61,11 @@ addBtn.addEventListener("click",function(e){
     const bookTitle = document.getElementById("title").value
     const bookAuthor = document.getElementById("author").value
     const bookPages= document.getElementById("pages").value
-    let newBook = new Book(bookTitle, bookAuthor, bookPages)
+    const readBook = document.querySelector(".read").value    
+    let newBook = new Book(counter++, bookTitle, bookAuthor, bookPages,readBook)
     myLibrary.push(newBook)
     addBookToLibrary()
+    console.log(newBook.id)
 })
 
 showButton.addEventListener("click", () => {
